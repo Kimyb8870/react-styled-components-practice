31195ef74af87f66492e8f1eed2766a88097798a
kimyb8870 <qhd0084@gmail.com>
2021-02-28 00:34:08 +0900

    Set: initialize css

---
+++ src/global-styles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        box-sizing: border-box;
+        padding: 0;
+        font-family: 'Encode Sans Expanded', sans-serif;
+        list-style-type: none;
+    }
+`;
